46d0cfeb5e981ed62c1a00f5836e8c58a2808bda
DURODOLA-ABDULKABIR <adeneeyee@gmail.com>
2021-11-02 21:47:29 +0100

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by user on 02/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
